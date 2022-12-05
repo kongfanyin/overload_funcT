d32b7b41cb818afdfc4a8f6e5953ad65a8b849cd
kongfanyin <103018828+kongfanyin@users.noreply.github.com>
2022-12-05 14:04:37 +0800

    函数模板重载

--- overload_funcT/overload_funcT.cpp
+++ overload_funcT/overload_funcT.cpp
@@ -1,1 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//using reloaded template functions
+
+
+#include<string>
+#include<fstream>
+#include<iostream>
+
+template <typename T>//origianl template
+void Swap(T &a, T &b);
+
+template <typename T>//new reloaded functon templates
+void Swap(T &a, T &b,int n);
+
+int main(char argc, char** argv)
+{
+
+
+
+
+	return 0;
+}
+template <typename T>
+void Swap(T &a, T &b)
+{
+
+
+}
+template <typename T>
+void Swap(T *a, T *b, int n)
+{
+
+
+}
